cc412a282e4129712815ce20d437cb9b85704cf4
Buckinzz <buckpendleton@gmail.com>
2021-01-21 11:06:51 -0500

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,26 @@
+function foo() {
+    if(true) {
+        const arr = [1,3,5];
+    for ( const ele for arr) {
+            console.log(ele);
+    }
+    } else {
+        console.log("something else")
+    }
+} 
+    //lalalala
+    // ? this is a function that does w/e
+
+    // ! warning this throws an error that must be caught
+
+const hello = "hello"
+const y = "world"
+
+console.log(hello + ' ' + y)
+console.log(hello);
+
+hello world
+
+
+hello world
+hello world
